92351350cf24c346572bf6f21a160d4d6797f243
Veyniac <tbhalla01@gmail.com>
2020-04-17 14:42:27 +0530

    Cleaning up website and Understanding how it is responsive

--- js/main.js
+++ js/main.js
@@ -346,7 +346,13 @@
             }
         });
     };
-
+    
+    
+    $(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+    });
 
    /* Initialize
     * ------------------------------------------------------ */
